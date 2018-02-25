647fa2fb4af4a94577110639971d0d3406db4e5c
Shang-Hung Tsai <shtsai7@gmail.com>
2018-02-25 16:46:03 -0500

    change route for creating user

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var user = require('../controllers/user');
 
-router.post('/', user.create);
+router.post('/create', user.create);
 router.get('/:id', user.findOne);
 router.put('/:id', user.update);
 router.delete('/:id', user.delete)
